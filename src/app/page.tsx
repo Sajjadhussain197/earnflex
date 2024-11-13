6af0b5c1319be0e0eecb34659d3f5c91199d4509
Sajjadhussain197 <sajjadhussain@9582@gmail.com>
2024-11-14 01:04:10 +0500

    updated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import TableShrimer from '@/components/loading/TalbeShrimmer';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/user-list');
+  }, []);
+
   return (
     <div className="bg-white text-black">
-      main
-     
+      <TableShrimer/>
     </div>
   );
 }
